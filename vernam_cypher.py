f48215c904781fd6e1be0ec3f54e1cb2b151ae5b
tsikorksi <16906214+tsikorksi@users.noreply.github.com>
2018-09-18 11:17:42 +0100

    finished vernam cipher

--- vernam_cypher.py
+++ vernam_cypher.py
@@ -5,40 +5,29 @@ import string
 
 def encode(input_string):
     """
-    function to execute OTP cipher encoding, by converting to binary, xoring and then converting back
-    the conversions are harder then the actual algorithm tbh
-    :param input_string: the string to be encoded
-    :return: the cipher and the pad
+    i gave up on binary, so here is the vernam cipher with ords and ascii
+    https://en.wikipedia.org/wiki/One-time_pad#Example
+    the math shenanigans is to deal with the fact that it is too easy to overflow ascii, so convert to simple a=0
+    then do the mod then add the numbers back, should probably split it up
+    :param input_string: the string to be coded
+    :return: the ciphered text, as well as the pad
     """
-    input_string = input_string.lower().replace(' ', '')
-    binary_string = (' '.join(format(ord(x), 'b') for x in input_string)).replace(' ', '')
-    # print(input_string, binary_string)
-
-    pad_string = (generate_pad(len(input_string))).replace(' ', '')
-    binary_pad = (' '.join(format(ord(x), 'b') for x in pad_string)).replace(' ', '')
-    # print(pad_string, binary_pad)
-
+    input_string = input_string.lower()
+    pad_string = generate_pad(len(input_string))
     cipher_text = ''
-    for i in range(0, len(binary_string)):
-        if int(binary_string[i]) + int(binary_pad[i]) == 1:
-            cipher_text += '1'
-        else:
-            cipher_text += '0'
-    print(cipher_text)
-
-    output_string = ''
-    for i in range(0, len(cipher_text), 8):
-        # todo: convert cipher text back to string
-        output_string += chr(int(cipher_text[i:i+8], base=2))
-    print(output_string)
+    # print(pad_string)
+    # print(input_string)
+    for i in range(0, len(input_string)):
+        cipher_text += chr((((ord(input_string[i])-97) + (ord(pad_string[i]))-97) % 26) + 97)
+    print(cipher_text, pad_string)
     return cipher_text, pad_string
 
 
 def generate_pad(count):
     """
-    generates a list of ascii letters, lowercase
-    :param count: the number of letters
-    :return: the l chars as 1 string
+    generates long string of lowercase ascii chars
+    :param count: the number of chars
+    :return: a string filled with random lowercase ascii chars
     """
     return ''.join(random.choices(string.ascii_lowercase, k=count))
 
