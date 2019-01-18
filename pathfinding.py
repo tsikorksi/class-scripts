d20cd1e61234b23a615896c31a8eebbc4dc9a936
tsikorksi <16906214+tsikorksi@users.noreply.github.com>
2019-01-18 09:40:59 +0000

    adding pathfinding

---
+++ pathfinding.py
@@ -0,0 +1,88 @@
+
+def min_finder(distance, unvisited, nodes):
+    """
+    finds min of all nodes that are also in unvisited
+    :param distance:  the array containing distances
+    :param unvisited: the unvisited nodes
+    :param nodes: all nodes keys
+    :return: the lowest value in distance whose key equivalent in nodes is in unvisited
+    """
+    temp = 999999
+    value = ''
+    for i in range(0, len(distance)):
+        if distance[i] < temp and nodes[i] in unvisited:
+            value = nodes[i]
+            temp = distance[i]
+    return value
+
+
+def parser(nodes, previous, start, end):
+    """
+    takes the calculated values and works out the full route
+    :param nodes: the list of nodes
+    :param previous: the master node for each node
+    :param start: the root node
+    :param end: the destination node
+    :return: the final route
+    """
+    done = False
+    route = end
+    while not done:
+        # adds the node that came before the later one to the total route
+        route = previous[nodes.index(route[0])] + route
+        # once the start node has been added, the loop ends
+        if route[0] == start:
+            done = True
+    return route
+
+
+def dijkstra(graph, start, end):
+    """
+    Dijkstra's algorithm solver
+    :param graph: the graph to be solved
+    :param start: the start node
+    :param end: the destination node
+    :return: the route from start to end and that distance
+    """
+    # list of all nodes, does not change
+    nodes = list(graph.keys())
+    # list of unvisited nodes, will be emptied as we go
+    unvisited = list(graph.keys())
+    # list of distances to a node
+    distance = []
+    # list of routes to each node
+    previous = []
+
+    # note that distance, nodes and previous use the same indexes, and are equivalent to 1 list
+
+    # fills distance and previous with the default values
+    for i in range(0, len(graph)):
+        distance.append(999999)
+        previous.append('')
+
+    # sets the start nodes distance to 0, to kickstart algorithm
+    distance[nodes.index(start)] = 0
+
+    # until unvisited is empty
+    while unvisited:
+        # the key for the min value in unvisited
+        u = min_finder(distance, unvisited, nodes)
+
+        unvisited.remove(u)
+
+        # key: the char for the values in the sub-dict graph[u], dist is the associated distance
+        for key, dist in graph[u].items():
+            # the possible shorter distance
+            alternate = distance[nodes.index(u)] + dist
+            # checks if the new distance is quicker the the recorded one
+            if alternate < distance[nodes.index(key)]:
+                # replaces the distance in the list if so
+                distance[nodes.index(key)] = alternate
+                # replaces the last values in the previous list with the node travelled from
+                previous[nodes.index(key)] = u
+
+    return parser(nodes, previous, start, end), distance[nodes.index(end)]
+
+
+def a_star(graph, heuristics, start, end):
+    return ()
---
+++ test_pathfinding.py
@@ -0,0 +1,71 @@
+from pathfinding import a_star
+from pathfinding import dijkstra
+
+import unittest
+
+
+class DijkstraTest(unittest.TestCase):
+    def test_basic(self):
+        """
+        the basic case tested for, plus bonus variations
+        """
+        graph = {
+            'a': {'b': 1, 'c': 2},
+            'b': {'a': 1, 'c': 2, 'd': 3},
+            'c': {'a': 2, 'b': 2, 'd': 1},
+            'd': {'b': 3, 'c': 1}
+        }
+        self.assertEqual(('acd', 3), dijkstra(graph, 'a', 'd'))
+        self.assertEqual(('dca', 3), dijkstra(graph, 'd', 'a'))
+        self.assertEqual(('bd', 3), dijkstra(graph, 'b', 'd'))
+
+    def test_simple1(self):
+        """
+        the graph for one of the div exercises, in two variations
+        """
+        graph = {
+            'a': {'b': 5, 'd': 3, 'c': 3},
+            'b': {'a': 5, 'f': 2},
+            'c': {'a': 3, 'f': 3, 'e': 3},
+            'd': {'a': 3, 'e': 2},
+            'e': {'d': 2, 'c': 3},
+            'f': {'b': 2, 'c': 3}
+        }
+        self.assertEqual(('ade', 5), dijkstra(graph, 'a', 'e'))
+        self.assertEqual(('acf', 6), dijkstra(graph, 'a', 'f'))
+
+    def test_weird_cases(self):
+        graph = {
+            'a': {}
+        }
+        self.assertEqual(('a', 0), dijkstra(graph, 'a', 'a'))
+        graph = {
+            'a': {'b': 0, 'c': 0},
+            'b': {'a': 0, 'c': 0, 'd': 0},
+            'c': {'a': 0, 'b': 0, 'd': 0},
+            'd': {'b': 0, 'c': 0}
+        }
+        self.assertEqual(('abd', 0), dijkstra(graph, 'a', 'd'))
+        self.assertEqual(('bd', 0), dijkstra(graph, 'b', 'd'))
+
+
+class AStarTest(unittest.TestCase):
+
+    def test_a_star(self, start, end, expected):
+        heuristics = {'a': 5, 'b': 3, 'c': 9, 'd': 0}
+        graph = {'a': {'b': 1, 'c': 2},
+                 'b': {'a': 1, 'c': 2, 'd': 3},
+                 'c': {'a': 2, 'b': 2, 'd': 1},
+                 'd': {'b': 3, 'c': 1}}
+        # arrange
+        # assert
+        self.assertEqual(('abd', 4), a_star(graph, heuristics, start, end))
+
+
+# TESTS
+
+
+# dijkstra
+test_dijkstra(graph, 'a', 'd', ('acd', 3))
+# a star
+test_a_star(graph, heuristics, 'a', 'd', ('abd', 4))
